30b57dd2183fb20065d1fcd954e49951bc1d2d70
Saksham Singh <iamsaksham369@gmail.com>
2026-03-15 13:04:50 +0530

    python commit

---
+++ CHAPTER 1 - PS/901/aisehi.py
@@ -0,0 +1,23 @@
+name="sakshsam"
+friend="uddhav"
+another_friend="sarthak"
+apple='''he said ,
+hello saksham 
+hey i am uddhav
+i want to eat an apple'''
+print("hello,"+name+"! bro have two friends named "+friend+" and "+another_friend)
+#print(apple)
+
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+
+#USING FOR LOOP
+for char in apple:
+    print(char)
